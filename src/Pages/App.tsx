a6a8ef7377700498484183aad2eb634da786bc52
pabloverly <pablopierre.pv@gmail.com>
2022-09-05 13:16:32 -0300

    05-09-2022

---
+++ src/Pages/App.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import Table from '../../src/Components/index'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"  style={{"marginTop":"5%","display":"grid", "justifyItems":"center"}}>
+      <Table 
+        a1={'verde'} 
+        a2={'verde'} 
+        a3={'verde'}
+        a4={'verde'}
+      
+
+        b1={'preto'} 
+        b2={'preto'} 
+        b3={'preto'}
+        b4={'preto'}
+        
+
+        c1={'c1'} 
+        c2={'c2'} 
+        c3={'c3'}
+        c4={'c4'}
+       
+
+        d1={'d1'} 
+        d2={'d2'} 
+        d3={'d3'}
+        d4={'d4'}
+        
+
+        e1={'e1'} 
+        e2={'e2'} 
+        e3={'e3'}
+        e4={'e4'}
+        
+
+        f1={'f1'} 
+        f2={'f2'} 
+        f3={'f3'}
+        f4={'f4'}
+       
+
+        g1={'g1'} 
+        g2={'g2'} 
+        g3={'g3'}
+        g4={'g4'}
+        
+      />
+
+    <Table 
+        a1={'azul'} 
+        a2={'azul'} 
+        a3={'azul'}
+        a4={'azul'}    
+
+        b1={'vermelho'} 
+        b2={'vermelho'} 
+        b3={'vermelho'}
+        b4={'vermelho'}       
+
+        c1={'c1'} 
+        c2={'c2'} 
+        c3={'c3'}
+        c4={'c4'}        
+
+        d1={'d1'} 
+        d2={'d2'} 
+        d3={'d3'}
+        d4={'d4'}       
+
+        e1={'e1'} 
+        e2={'e2'} 
+        e3={'e3'}
+        e4={'e4'}        
+
+        f1={'f1'} 
+        f2={'f2'} 
+        f3={'f3'}
+        f4={'f4'}        
+
+        g1={'g1'} 
+        g2={'g2'} 
+        g3={'g3'}
+        g4={'g4'}
+       
+      />
+    </div>
+  );
+}
+
+export default App;
